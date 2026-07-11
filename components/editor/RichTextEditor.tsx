02951b1d4cceea54bfbeb629faa47f8d1306ebac
Ruveendra Dewshan <ruveendradeewshan@gmail.com>
2026-07-11 02:05:43 -0400

    feat: set up initial Tiptap editor

--- components/editor/RichTextEditor.tsx
+++ components/editor/RichTextEditor.tsx
@@ -1,3 +1,30 @@
+"use client";
+
+import { useEditor, EditorContent } from '@tiptap/react'
+import StarterKit from '@tiptap/starter-kit'
+import Placeholder from "@tiptap/extension-placeholder";
+
 export function RichTextEditor() {
-  return null;
+
+  const editor = useEditor({
+    extensions: [StarterKit,
+        Placeholder.configure({
+        placeholder: "Type here...",
+        showOnlyWhenEditable: true,
+        showOnlyCurrent: false,
+      }),
+    ],
+    
+    // Don't render immediately on the server to avoid SSR issues
+    content: "",
+    immediatelyRender: false,
+  })
+
+  return (
+    <div className="editor-shell">
+      <EditorContent editor={editor} className="editor-content" />
+    </div>
+  )
+
 }
+export default RichTextEditor
